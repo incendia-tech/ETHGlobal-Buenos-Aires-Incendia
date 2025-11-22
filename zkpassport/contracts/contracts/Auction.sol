712229db52f998d0e8e418820b0c6ab278947e1a
pardis-toolabi <pardis.tlbi@gmail.com>
2025-11-22 11:21:58 -0300

    chore: init auction

---
+++ zkpassport/contracts/contracts/Auction.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+import "./verifier.sol";
+
+contract Auction {
+   Groth16Verifier public verifier;
+
+   error InvalidProof();
+
+    uint256 public ceremonyId;
+
+    uint256 public biddingDeadline;
+    uint256 public bidSubmissionDeadline;
+    uint256 public resultDeadline;
+    
+
+
+    uint256[] winningBids;
+    address[] winners;
+
+    event BidSubmitted(address indexed bidder, uint256 bid);
+
+
+    bool private initialized = false;
+    mapping(uint256 => bool) public usedNullifiers;
+
+// dont need a construtor since its deployed through factory
+    function initialize(
+        address _verifier,
+        uint256 _biddingDeadline,
+        uint256 _submissionDeadline,
+        uint256 _resultDeadline,
+        uint256 _ceremonyId,
+        uint256 _maxWinners
+    ) external {
+        verifier = Groth16Verifier(_verifier);
+        biddingDeadline = _biddingDeadline;
+        bidSubmissionDeadline = _submissionDeadline;
+        resultDeadline = _resultDeadline;
+        ceremonyId = _ceremonyId;
+        winners = new address[](_maxWinners);
+        winningBids = new uint256[](_maxWinners);
+        initialized = true;
+    }
+
+
+}
